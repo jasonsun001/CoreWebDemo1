ff80258d5f6d367d857c3dd4cca07a401134c06b
jason <1479235292@qq.com>
2019-11-12 10:53:04 +0800

    commit1

--- src/main/java/com/springmvc/HelloController.java
+++ src/main/java/com/springmvc/HelloController.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author 14792
+ */
 @Controller
 @RequestMapping("/hello")
 public class HelloController{
+    private static final Logger logger = Logger.getLogger("HelloController");
     @RequestMapping(method = RequestMethod.GET)
     public String printHello(ModelMap model) {
+        logger.log(Level.INFO, "message from /hello");
         model.addAttribute("message", "Hello Spring MVC Framework!");
-        return "hey/hello";
+        return "/hello";
     }
 }
