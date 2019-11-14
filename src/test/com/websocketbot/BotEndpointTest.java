5bd0376e17842447a10c789408067e3f0d1d3186
jason <1479235292@qq.com>
2019-11-14 15:29:12 +0800

    One commit

---
+++ src/test/com/websocketbot/BotEndpointTest.java
@@ -0,0 +1,36 @@
+package com.websocketbot;
+
+class BotEndpointTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void openConnection() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void message() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void closedConnection() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void error() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sendAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUserList() {
+    }
+}
